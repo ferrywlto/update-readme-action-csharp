01c33148223ac7a63ec3ef52263f11645c10680a
Ferry To <ferry@verdantsparks.io>
2022-03-04 16:07:01 +0800

    check file path on production

--- .github/actions/action1/Program.cs
+++ .github/actions/action1/Program.cs
@@ -1,2 +1,23 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine($"Hello, {(string.IsNullOrEmpty(args[0]) ? "Maid" : args[0])}!");
+Console.WriteLine($"args: {args.Length}");
+
+if(args.Length > 0) {
+
+    Console.WriteLine($"Hello, {(string.IsNullOrEmpty(args[0]) ? "Maid" : args[0])}!");
+
+    if(File.Exists(args[0]))
+    {
+        var fileInfo = new FileInfo(args[0]);
+        Console.WriteLine($"File: {fileInfo.FullName}");
+    }
+    else if(Directory.Exists(args[0])) {
+        var dirInfo = new DirectoryInfo(args[0]);
+        Console.WriteLine($"Directory: {dirInfo.FullName}");
+    }
+}
+
+
+
+
+var time = DateTime.Now;
+Console.WriteLine($"::set-output name=time::{time}");
--- .github/actions/action1/entrypoint.sh
+++ .github/actions/action1/entrypoint.sh
@@ -1,5 +1,8 @@
 #!/bin/sh -l
 
-echo "@@@ Hello $1"
-time=$(date)
-echo "::set-output name=time::$time"
+# echo "@@@ Hello $1"
+# time=$(date)
+# echo "::set-output name=time::$time"
+
+# exec ls | $@
+dotnet /app/action1.dll README.md
